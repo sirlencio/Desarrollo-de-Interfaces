58e3d1aaee9cd4aabe814768a70c5dbbe95ff530
sirlencio <carlos.l.p.251001@gmail.com>
2022-10-10 19:45:06 +0200

    Boton borrar y ocultos

--- Ventana tabla/Ventana tabla/Form1.Designer.cs
+++ Ventana tabla/Ventana tabla/Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,11 +46,13 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dataGridView1.Location = new System.Drawing.Point(132, 42);
+            this.dataGridView1.Location = new System.Drawing.Point(73, 79);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(243, 342);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
             // 
             // Column1
             // 
@@ -61,7 +67,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(526, 89);
+            this.button1.Location = new System.Drawing.Point(526, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 69);
             this.button1.TabIndex = 1;
@@ -72,7 +78,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(526, 251);
+            this.button2.Location = new System.Drawing.Point(526, 183);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(89, 69);
             this.button2.TabIndex = 2;
@@ -80,11 +86,53 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(526, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Baja";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(63, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filtrar:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(378, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -93,6 +141,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,5 +152,9 @@
         private Button button2;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
     }
 }
--- Ventana tabla/Ventana tabla/Form1.cs
+++ Ventana tabla/Ventana tabla/Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
 namespace Ventana_tabla
 {
     public partial class Form1 : Form
@@ -27,6 +32,61 @@ namespace Ventana_tabla
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.SelectedRows[0].Index;
+            dataGridView1.Rows.RemoveAt(index);
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[0].IsNewRow)
+            {
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DataGridView filtro = new DataGridView();
+            DataGridView aux = new DataGridView();
+            aux = dataGridView1;
+            filtro.Columns.Add("0", "Nombre");
+            filtro.Columns.Add("0", "betoven");
+            filtro.Rows.Add("Pepe", "9");
+            string busqueda = textBox1.Text;
+            dataGridView1.Rows.Clear();
+            foreach (DataGridViewRow r in filtro.SelectedRows)
+            {
+                int index = dataGridView1.Rows.Add(r.Clone() as DataGridViewRow);
+                foreach (DataGridViewCell o in r.Cells)
+                {
+                    dataGridView1.Rows[index].Cells[o.ColumnIndex].Value = o.Value;
+                }
+            }
+            /* for (int i = 0; i < dataGridView1.Rows.Count; i++)
+             {
+                 string nombre = dataGridView1.Rows[i].Cells[0].Value + "";
+                 if (nombre.Contains(busqueda))
+                 {
+                     String not = dataGridView1.Rows[i].Cells[1].Value + "";
+
+                     //filtro.Rows.Add(nombre, not);
+                     dataGridView1.DataSource = filtro;
+                     label2.Text = nombre;
+                 }
+             }*/
         }
     }
 }
