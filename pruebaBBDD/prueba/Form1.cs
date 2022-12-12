7e332677467652af940bc54f1141df0863c9e6e0
sirlencio <carlos.l.p.251001@gmail.com>
2022-12-12 20:44:38 +0100

    initial

---
+++ pruebaBBDD/prueba/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace prueba
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection sql;
+        public Form1()
+        {
+            InitializeComponent();
+            sql = new MySqlConnection("server=localhost;user id=root;database=examen;password=admin");
+            cargarTabla();
+        }
+        private void cargarTabla()
+        {
+            sql.Open();
+            MySqlCommand comando = new MySqlCommand("Select * from articulos", sql);
+            MySqlDataAdapter ad = new MySqlDataAdapter(comando);
+            DataTable t = new DataTable();
+            ad.Fill(t);
+            dataGridView1.DataSource = t;
+            sql.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(dataGridView1);
+            form2.ShowDialog();
+            cargarTabla();
+        }
+    }
+}
---
+++ pruebaBBDD/prueba/Form2.cs
@@ -0,0 +1,62 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba
+{
+    public partial class Form2 : Form
+    {
+        DataGridView d;
+        MySqlConnection sql;
+        public Form2(DataGridView d)
+        {
+            InitializeComponent();
+            this.d = d;
+            sql = new MySqlConnection("server=localhost;user id=root;database=examen;password=admin");
+            cargaFamilias();
+        }
+        private void cargaFamilias()
+        {
+            sql.Open();
+            MySqlCommand comando = new MySqlCommand("Select familia from familias", sql);
+            MySqlDataReader reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader.GetString(0));
+            }
+            sql.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string codigo, descripcion, familia;
+            float precio;
+            int stock, codfam;
+
+            codigo = textBox1.Text;
+            descripcion = textBox2.Text;
+            precio = float.Parse(textBox3.Text);
+            stock = Int32.Parse(textBox4.Text);
+
+            familia = comboBox1.Text;
+            sql.Open();
+            MySqlCommand select = new MySqlCommand("Select codigo from familias where Familia = '"+ familia + "'", sql);
+            MySqlDataReader reader = select.ExecuteReader();
+            reader.Read();
+            codfam = reader.GetInt32(0);
+            sql.Close();
+            sql.Open();
+            MySqlCommand añadir = new MySqlCommand("Insert into articulos values ('"+ codigo + "','" + descripcion + "'," + precio + "," + stock + "," + codfam + ")", sql);
+            añadir.ExecuteReader(0);
+            sql.Close();
+            this.Close();
+        }
+    }
+}
